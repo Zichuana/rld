25e86dea8dd9f5a04d58c0e58ef79ea8b1d182bc
Zichuana <2092653757@qq.com>
2023-03-12 11:29:41 +0800

    update rna to pro

--- stronghold.py
+++ stronghold.py
@@ -1,3 +1,27 @@
+rna_code = {'UUU': 'F', 'UUC': 'F',
+            'UUA': 'L', 'UUG': 'L', 'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+            'AUU': 'I', 'AUC': 'I', 'AUA': 'I',
+            'AUG': 'M',
+            'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+            'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+            'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+            'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+            'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+            'UAU': 'Y', 'UAC': 'Y',
+            'CAU': 'H', 'CAC': 'H',
+            'CAA': 'Q', 'CAG': 'Q',
+            'AAU': 'N', 'AAC': 'N',
+            'AAA': 'K', 'AAG': 'K',
+            'GAU': 'D', 'GAC': 'D',
+            'GAA': 'E', 'GAG': 'E',
+            'UGU': 'C', 'UGC': 'C',
+            'UGG': 'W',
+            'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 'AGA': 'R', 'AGG': 'R',
+            'AGU': 'S', 'AGC': 'S',
+            'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+            'UAA': '', 'UAG': '', 'UGA': ''}
+
+
 def Counting_DNA_Nucleotides(x: str):
     A = x.count('A')
     C = x.count('C')
@@ -47,3 +71,22 @@ def Counting_Point_Mutations(x: str, y: str):
         if i != j:
             dh += 1
     return dh
+
+
+def Translating_RNA_into_Protein(x: str):
+    lenx = len(x)
+    res = ''
+    for i in range(1, lenx + 1, 3):
+        s = x[i - 1] + x[i] + x[i + 1]
+        res += rna_code[s]
+    return res
+
+
+def Finding_a_Motif_in_DNA(s: str, t: str):
+    lent, lens = len(t), len(s)
+    res = []
+    for i in range(1, lens + 1 - lent):
+        mid = s[i - 1:i + lent - 1]
+        if mid == t:
+            res.append(i)
+    return res
